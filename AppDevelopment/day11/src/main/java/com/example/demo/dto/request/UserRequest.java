b0e541f0e4f39c45013c13409000e0751a4fd109
Priyadharshini2901 <144902916+Priyadharshini2901@users.noreply.github.com>
2023-10-05 14:48:13 +0530

    Add files via upload

---
+++ AppDevelopment/day11/src/main/java/com/example/demo/dto/request/UserRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto.request;
+
+
+
+
+import com.example.demo.model.enumerate.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String name;
+    private String email;
+    private String password;
+    private Boolean isEnabled;
+    private Role role;
+}
