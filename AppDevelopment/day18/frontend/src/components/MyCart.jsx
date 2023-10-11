51fb2ad6a613085f24977bd851df2a5e6e54db55
Priyadharshini2901 <144902916+Priyadharshini2901@users.noreply.github.com>
2023-10-11 15:10:30 +0530

    Add files via upload

---
+++ AppDevelopment/day18/frontend/src/components/MyCart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import Dashboard from './Dashboard'
+function MyCart() {
+  return (
+    <>
+    <Dashboard/>
+
+    <div className="text-center">
+        <div className="font-bold">
+        {/* <h1>My Cart</h1> */}
+        </div>
+    </div>
+
+    </>
+      )
+}
+
+export default MyCart
---
+++ AppDevelopment/day18/frontend/src/components/Settings.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+
+function Settings() {
+  return (
+    <>
+   <Dashboard/>
+
+    <div className="text-center">
+        <div className="font-bold">
+        {/* <h1>Settings</h1> */}
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Settings
